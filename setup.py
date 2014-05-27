a127e9cd89de70de4b7ae703eb08b04d77e3e828
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-05-27 18:31:40 +0500

    name fixed

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='snapchat-python',
+setup(name='snapchat',
       version='0.1',
       description='Implementation of the Snapchat protocol in Python',
       url='https://github.com/itsnauman/snapchat-python',
       author='Nauman Ahmad',
       author_email='nauman-ahmad@outlook.com',
       license='MIT',
-      packages=['snapchat-python'],
+      packages=['snapchat'],
       install_requires=[
           'requests==2.0.1',
           'pycrypto==2.6.1'
